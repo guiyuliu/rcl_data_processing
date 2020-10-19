fc32069d6dec7d73b00d4a16c795ce3640c2a910
guiyuliu <liuguiyuok@163.com>
2020-10-19 23:03:21 +0800

    geometry_utils form nuscenes devkit

---
+++ geometry_utils.py
@@ -0,0 +1,67 @@
+from enum import IntEnum
+from typing import Tuple
+
+import numpy as np
+from pyquaternion import Quaternion
+
+# this file is from nuscenes/utils/geometry_utils.py 
+
+def view_points(points: np.ndarray, view: np.ndarray, normalize: bool) -> np.ndarray:
+    """
+    This is a helper class that maps 3d points to a 2d plane. It can be used to implement both perspective and
+    orthographic projections. It first applies the dot product between the points and the view. By convention,
+    the view should be such that the data is projected onto the first 2 axis. It then optionally applies a
+    normalization along the third dimension.
+    For a perspective projection the view should be a 3x3 camera matrix, and normalize=True
+    For an orthographic projection with translation the view is a 3x4 matrix and normalize=False
+    For an orthographic projection without translation the view is a 3x3 matrix (optionally 3x4 with last columns
+     all zeros) and normalize=False
+    :param points: <np.float32: 3, n> Matrix of points, where each point (x, y, z) is along each column.
+    :param view: <np.float32: n, n>. Defines an arbitrary projection (n <= 4).
+        The projection should be such that the corners are projected onto the first 2 axis.
+    :param normalize: Whether to normalize the remaining coordinate (along the third axis).
+    :return: <np.float32: 3, n>. Mapped point. If normalize=False, the third coordinate is the height.
+    """
+
+    assert view.shape[0] <= 4
+    assert view.shape[1] <= 4
+    assert points.shape[0] == 3
+
+    viewpad = np.eye(4)
+    viewpad[:view.shape[0], :view.shape[1]] = view
+
+    nbr_points = points.shape[1]
+
+    # Do operation in homogenous coordinates.
+    points = np.concatenate((points, np.ones((1, nbr_points))))
+    points = np.dot(viewpad, points)
+    points = points[:3, :]
+
+    if normalize:
+        points = points / points[2:3, :].repeat(3, 0).reshape(3, nbr_points)
+
+    return points
+
+
+def transform_matrix(translation: np.ndarray = np.array([0, 0, 0]),
+                     rotation: Quaternion = Quaternion([1, 0, 0, 0]),
+                     inverse: bool = False) -> np.ndarray:
+    """
+    Convert pose to transformation matrix.
+    :param translation: <np.float32: 3>. Translation in x, y, z.
+    :param rotation: Rotation in quaternions (w ri rj rk).
+    :param inverse: Whether to compute inverse transform matrix.
+    :return: <np.float32: 4, 4>. Transformation matrix.
+    """
+    tm = np.eye(4)
+
+    if inverse:
+        rot_inv = rotation.rotation_matrix.T
+        trans = np.transpose(-np.array(translation))
+        tm[:3, :3] = rot_inv
+        tm[:3, 3] = rot_inv.dot(trans)
+    else:
+        tm[:3, :3] = rotation.rotation_matrix
+        tm[:3, 3] = np.transpose(np.array(translation))
+
+    return tm
